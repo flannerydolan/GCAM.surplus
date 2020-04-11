fa4c7c29c4328a4b4ff9e4d1094e398906c06e5a
flannerydolan <flannery.dolan@tufts.edu>
2020-04-11 17:05:31 -0500

    Add files via upload
    fix figure 3 code

--- R/Figure3.R
+++ R/Figure3.R
@@ -7,24 +7,31 @@
 # Generate Figure 3
 
 #################### load data
-load('data/crops_j.rda')
+load('data/crops.rda')
 load('data/indus_price.rda')
 load('data/ap_with.rda')
-#########################
 
-p1<-ggplot(crops_j,aes(value,color=as.factor(tax)))+stat_ecdf(aes(linetype=`Scenario Type`),geom="step")+
-  scale_color_manual(name="Tax",values=c("orange","cyan"),labels=c("FFICT","UCT"))+xlab(bquote("Area (thousand km"^"2"~")"))+
-  ylab('Probability')+xlim(240000,320000)+theme_classic()+
-  theme(plot.title = element_text(hjust = 0, size = 18))+theme(legend.position = "none") 
+
+
+crops %>% process_query()->crops
+
+crops %>% filter(landleaf=="Crop") -> crops
+
+#########################
+p1<-ggplot(crops,aes(value/1000,fill=as.factor(tax)))+geom_density(alpha=0.4)+
+  scale_fill_manual(name="Tax",values=c("orange","cyan"),labels=c("FFICT","UCT"))+xlab(bquote("Area (million km"^"2"~")"))+
+  theme_classic()+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0))+
+  theme(plot.title = element_text(hjust = 0, size = 18))+theme(legend.position = "none",axis.text=element_text(size=14),text=element_text(size=14))
+
 
 p2<-ggplot(indus_price,aes(value,fill=as.factor(tax)))+geom_density(alpha=.4)+
   scale_fill_manual(name="Tax",values=c("orange","cyan"),labels=c("FFICT","UCT"))+
   theme_classic()+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0))+
-  xlab(bquote("$/m"^"3"))+theme(legend.position=c(.75,.75))
+  xlab(bquote("$/m"^"3"))+theme(legend.position=c(.75,.75),axis.text=element_text(size=14),text=element_text(size=14))
 
 p3<-ggplot(ap_with,aes(value,fill=as.factor(tax)),alpha=.04)+geom_density(alpha=.4)+
   scale_fill_manual(name="Tax",values=c("orange","cyan"),labels=c("FFICT","UCT"))+
-  theme_classic()+theme(legend.position = "none")+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0))+
+  theme_classic()+theme(legend.position = "none",axis.text=element_text(size=14),text=element_text(size=14))+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0))+
   xlab(bquote("km"^"3"))
 
 
@@ -32,12 +39,16 @@ p3<-ggplot(ap_with,aes(value,fill=as.factor(tax)),alpha=.04)+geom_density(alpha=
 
 gt <- arrangeGrob(p1,                               # bar plot spaning two columns
                   p3, p2,                               # box plot and scatter plot
-                  ncol = 2, nrow = 2, 
-                  layout_matrix = rbind(c(1,2), c(1,3)))
+                  ncol = 1, nrow = 3,
+                  #layout_matrix = rbind(c(1,2), c(1,3)))
+                  layout_matrix = rbind(c(1,2,3)))
 
 
 p<-as_ggplot(gt) +                                # transform to a ggplot
-  draw_plot_label(label = c("A", "B", "C"), size = 15,x = c(0, 0.5, 0.5), y = c(1, 1, 0.5)) # Add labels
+  draw_plot_label(label = c("A", "B", "C"), size = 15,x = c(0, 0.33, 0.66), y = c(1, 1, 1)) # Add labels
 
 
+
+svg('Figure3.svg',height=3,width=10)
 p
+dev.off()
